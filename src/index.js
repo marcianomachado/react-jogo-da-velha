5d35747d7785cc6a5dddfedc115399042cd36b43
Marciano Machado <marcianomachado@gmail.com>
2019-10-17 23:39:40 -0300

    Mostrando as Jogadas Anteriores

--- src/index.js
+++ src/index.js
@@ -84,6 +84,17 @@ class Game extends React.Component {
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Ir para o movimento #' + move :
+                'Iniciar Jogo';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
 
         if (winner) {
@@ -101,7 +112,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
