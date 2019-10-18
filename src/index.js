32fab2e6ede1f6648fb6a42f408c177ac358d9e6
Marciano Machado <marcianomachado@gmail.com>
2019-10-17 23:02:25 -0300

    Trocando Turnos

--- src/index.js
+++ src/index.js
@@ -17,12 +17,16 @@ import './index.css';
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true
         };
     }
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     renderSquare(i) {
@@ -35,7 +39,7 @@ import './index.css';
     }
   
     render() {
-      const status = 'Próximo Jogador: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
